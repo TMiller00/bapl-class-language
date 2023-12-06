c5ef7f6528ebd3534f1e343a9aaf3805e6e6ff19
TMiller00 <thomas@hmbradley.com>
2023-12-05 22:38:34 -0800

    11 Matching A Summation

--- lesson-1/homework.lua
+++ lesson-1/homework.lua
@@ -1,6 +1,16 @@
 local lpeg = require "lpeg"
+local luaunit = require "luaunit"
 
-local p = lpeg.P("hello")
+local space = lpeg.S(" ") ^ 0
+local digits = lpeg.R("09") ^ 1 * space
+local op = lpeg.P("+") * space
 
-print(lpeg.match(p, "hello world"))
-print(lpeg.match(p, "hy world"))
+local base = digits * (op * digits) ^ 1
+local p = space * base ^ 1 * space
+
+function TestPattern()
+  luaunit.assertEquals(p:match("1 + 1"), 6)
+  luaunit.assertEquals(p:match("10 + 22"), 8)
+end
+
+os.exit(luaunit.LuaUnit.run())
