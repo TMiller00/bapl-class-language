201dbe4ab2d57d597faafb3cbecee59b9b7aa444
TMiller00 <thomas@hmbradley.com>
2023-12-06 11:27:40 -0800

    Final Arithmetic Expressions

--- lesson-1/homework.lua
+++ lesson-1/homework.lua
@@ -6,6 +6,9 @@ local numeral = (lpeg.S("+-") ^ -1 * lpeg.R("09") ^ 1 / tonumber) * space
 local opA = lpeg.C(lpeg.S("+-")) * space
 local opM = lpeg.C(lpeg.S("*/%^")) * space
 
+local OP = "(" * space
+local CP = ")" * space
+
 local function fold(lst)
   local acc = lst[1]
 
@@ -30,15 +33,23 @@ local function fold(lst)
   return acc
 end
 
-local term = space * lpeg.Ct(numeral * (opM * numeral) ^ 0) / fold
-local sum = space * lpeg.Ct(term * (opA * term) ^ 0) / fold * -1
+local primary = lpeg.V "primary"
+local term = lpeg.V "term"
+local exp = lpeg.V "exp"
+
+g = lpeg.P { "exp",
+  primary = numeral + OP * exp * CP,
+  term = space * lpeg.Ct(primary * (opM * primary) ^ 0) / fold,
+  exp = space * lpeg.Ct(term * (opA * term) ^ 0) / fold,
+} * -1
 
 function TestSimplePattern()
-  luaunit.assertEquals(sum:match("2 * 2 + 6 * 10"), 64)
-  luaunit.assertEquals(sum:match("34 - 32 / 16"), 32)
-  luaunit.assertEquals(sum:match("123 + 456 +"), nil)
-  luaunit.assertEquals(sum:match("5 % 3 * 4"), 8)
-  luaunit.assertEquals(sum:match("4 ^ 4 / 2"), 128)
+  luaunit.assertEquals(g:match("2 * 2 + 6 * 10"), 64)
+  luaunit.assertEquals(g:match("34 - 32 / 16"), 32)
+  luaunit.assertEquals(g:match("123 + 456 +"), nil)
+  luaunit.assertEquals(g:match("5 % 3 * 4"), 8)
+  luaunit.assertEquals(g:match("4 ^ 4 / 2"), 128)
+  luaunit.assertEquals(g:match("2 * (2 + 6) * 10"), 160)
 end
 
 os.exit(luaunit.LuaUnit.run())
