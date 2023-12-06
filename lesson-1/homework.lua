dfba579555e00c15faa8e6649d384ef74a708ffb
TMiller00 <thomas@hmbradley.com>
2023-12-06 11:26:53 -0800

    19 Adding More Operators

--- lesson-1/homework.lua
+++ lesson-1/homework.lua
@@ -3,28 +3,42 @@ local luaunit = require "luaunit"
 
 local space = lpeg.S(" \n\t") ^ 0
 local numeral = (lpeg.S("+-") ^ -1 * lpeg.R("09") ^ 1 / tonumber) * space
-local op = lpeg.S("+") * space
+local opA = lpeg.C(lpeg.S("+-")) * space
+local opM = lpeg.C(lpeg.S("*/%^")) * space
 
 local function fold(lst)
-  local acc = 0
+  local acc = lst[1]
 
-  for i = 1, #lst do
-    acc = acc + lst[i]
+  for i = 2, #lst, 2 do
+    if lst[i] == "+" then
+      acc = acc + lst[i + 1]
+    elseif lst[i] == "-" then
+      acc = acc - lst[i + 1]
+    elseif lst[i] == "*" then
+      acc = acc * lst[i + 1]
+    elseif lst[i] == "/" then
+      acc = acc / lst[i + 1]
+    elseif lst[i] == "%" then
+      acc = acc % lst[i + 1]
+    elseif lst[i] == "^" then
+      acc = acc ^ lst[i + 1]
+    else
+      error("invalid character")
+    end
   end
 
   return acc
 end
 
-local p = space * lpeg.Ct(numeral * (op * numeral) ^ 0) / fold * -1
+local term = space * lpeg.Ct(numeral * (opM * numeral) ^ 0) / fold
+local sum = space * lpeg.Ct(term * (opA * term) ^ 0) / fold * -1
 
-function TestPattern()
-  luaunit.assertEquals(p:match("12 + 23 + 34 + 45 + 56"), 170)
-  luaunit.assertEquals(p:match("123 + 456 + 789"), 1368)
-  luaunit.assertEquals(p:match("123 + 456 +"), nil)
-
-  luaunit.assertEquals(p:match("123 + -456 + 789"), 456)
-  luaunit.assertEquals(p:match("+123 + -456 + +789"), 456)
-  luaunit.assertEquals(p:match("+123 + -456 + +789 +"), nil)
+function TestSimplePattern()
+  luaunit.assertEquals(sum:match("2 * 2 + 6 * 10"), 64)
+  luaunit.assertEquals(sum:match("34 - 32 / 16"), 32)
+  luaunit.assertEquals(sum:match("123 + 456 +"), nil)
+  luaunit.assertEquals(sum:match("5 % 3 * 4"), 8)
+  luaunit.assertEquals(sum:match("4 ^ 4 / 2"), 128)
 end
 
 os.exit(luaunit.LuaUnit.run())
