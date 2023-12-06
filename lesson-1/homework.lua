a7d80b5c498af2aa98e9e885f324c5415e18748b
TMiller00 <thomas@hmbradley.com>
2023-12-06 10:13:22 -0800

    13 Position Capture

--- lesson-1/homework.lua
+++ lesson-1/homework.lua
@@ -1,16 +1,19 @@
 local lpeg = require "lpeg"
 local luaunit = require "luaunit"
 
-local space = lpeg.S(" ") ^ 0
-local digits = lpeg.R("09") ^ 1 * space
-local op = lpeg.P("+") * space
+local digits = lpeg.R("09") ^ 1
+local op = lpeg.Cp() * lpeg.S("+")
 
-local base = digits * (op * digits) ^ 1
-local p = space * base ^ 1 * space
+local base = lpeg.C(digits) * op ^ -1
+local p = base ^ 1
 
 function TestPattern()
-  luaunit.assertEquals(p:match("1 + 1"), 6)
-  luaunit.assertEquals(p:match("10 + 22"), 8)
+  local twelve, three, thirteen, six, twentyfive = p:match("12+13+25")
+  luaunit.assertStrMatches(twelve, "12")
+  luaunit.assertStrMatches(three, "3")
+  luaunit.assertStrMatches(thirteen, "13")
+  luaunit.assertStrMatches(six, "6")
+  luaunit.assertStrMatches(twentyfive, "25")
 end
 
 os.exit(luaunit.LuaUnit.run())
