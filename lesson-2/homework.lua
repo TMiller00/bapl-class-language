985db8106465821053d7f516e53bb4c790072242
TMiller00 <thomas@hmbradley.com>
2023-12-11 20:24:16 -0800

    5 Hexadecimal Numbers

--- lesson-2/homework.lua
+++ lesson-2/homework.lua
@@ -1,15 +1,73 @@
--- 3 - Stack Machines
+local lpeg = require "lpeg"
+local luaunit = require "luaunit"
 
--- a) Manually execute the following stack-machine program
---
--- | push 4   |   |         |   |         |   |         |    |      |    |      |   |     |   |  |    |
--- | push 2   |   | push 2  |   |         |   |         |    |      |    |      |   |     |   |  |    |
--- | push 24  |   | push 24 |   | push 24 |   |         |    |      |    |      |   |     |   |  |    |
--- | push 21  |   | push 21 |   | push 21 |   | push 21 |    |      | 21 |      |   |     |   |  |    |
--- | sub      |   | sub     |   | sub     |   | sub     | 24 | sub  | 24 |      | 3 |     |   |  |    |
--- | mult     |   | mult    |   | mult    | 2 | mult    |  2 | mult |  2 | mult | 2 |     | 6 |  |    |
--- | add      |   | add     | 4 | add     | 4 | add     |  4 | add  |  4 | add  | 4 | add | 4 |  | 10 |
+-- Front End --------------------------------------
 
--- b) What arithmetic expression would generate that previous program?
+local space = lpeg.S(" \t\n") ^ 0
+local hexLeader = "0" * (lpeg.P("x") + lpeg.P("X"))
+local hexContent = (lpeg.R("af", "AF") + lpeg.R("09")) ^ -6
 
-print(4 + (2 * (24 - 21)))
+local hexadecimal = hexLeader * hexContent * -1 * space
+local numeral = lpeg.R("09") ^ 1 * -1 * space
+
+local function node(num)
+  return { tag = "number", val = tonumber(num) }
+end
+
+local numbers = space * (hexadecimal + numeral) / node
+
+local function parse(input)
+  return numbers:match(input)
+end
+
+-- Back End ---------------------------------------
+
+local function compile(ast)
+  if ast.tag == "number" then
+    return { "push", ast.val }
+  end
+end
+
+-- Execution --------------------------------------
+
+local function run(code, stack)
+  local pc = 1
+  local top = 0
+
+  while pc < #code do
+    if code[pc] == "push" then
+      pc = pc + 1
+      top = top + 1
+      stack[top] = code[pc]
+    else
+      error("unknown instruction")
+    end
+
+    pc = pc + 1
+  end
+end
+
+---------------------------------------------------
+
+local function testNumbers(input)
+  local stack = {}
+  local ast = parse(input)
+  local code = compile(ast)
+  run(code, stack)
+
+  return stack[1]
+end
+
+function TestNumbers()
+  luaunit.assertEquals(testNumbers("5"), 5)
+  luaunit.assertEquals(testNumbers("10"), 10)
+  luaunit.assertEquals(testNumbers("0x1"), 1)
+  luaunit.assertEquals(testNumbers("0x2A"), 42)
+  luaunit.assertEquals(testNumbers("0x3abc"), 15036)
+  luaunit.assertEquals(testNumbers("0x4ABCD"), 306125)
+  luaunit.assertEquals(testNumbers("0x5abcde"), 5946590)
+  luaunit.assertEquals(testNumbers("0x6F4F3F"), 7294783)
+  luaunit.assertEquals(testNumbers("    0X111"), 273)
+end
+
+os.exit(luaunit.LuaUnit.run())
