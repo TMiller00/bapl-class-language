db0881b118f7bea3f4055cd75980fde6c892a026
TMiller00 <thomas@hmbradley.com>
2023-12-12 14:46:03 -0800

    7 Adding Multiplication And Division

--- lesson-2/homework.lua
+++ lesson-2/homework.lua
@@ -3,29 +3,70 @@ local luaunit = require "luaunit"
 
 -- Front End --------------------------------------
 
+-- Spaces
 local space = lpeg.S(" \t\n") ^ 0
+
+-- Hexadecimals
 local hexLeader = "0" * (lpeg.P("x") + lpeg.P("X"))
 local hexContent = (lpeg.R("af", "AF") + lpeg.R("09")) ^ -6
+local hexadecimal = hexLeader * hexContent * space
 
-local hexadecimal = hexLeader * hexContent * -1 * space
-local numeral = lpeg.R("09") ^ 1 * -1 * space
+-- Integers
+local numeral = lpeg.R("09") ^ 1 * -lpeg.S("Xx") * space
 
+-- All Numbers
 local function node(num)
   return { tag = "number", val = tonumber(num) }
 end
 
-local numbers = space * (hexadecimal + numeral) / node
+local numbers = (hexadecimal + numeral) / node
+
+-- Operators
+local opA = lpeg.C(lpeg.S("+-")) * space
+local opM = lpeg.C(lpeg.S("*/")) * space
+
+local function foldBin(lst)
+  local tree = lst[1]
+
+  for i = 2, #lst, 2 do
+    tree = { tag = "binop", e1 = tree, op = lst[i], e2 = lst[i + 1] }
+  end
+
+  return tree
+end
+
+local exp = space * lpeg.Ct(numbers * (opM * numbers) ^ 0) / foldBin
 
 local function parse(input)
-  return numbers:match(input)
+  return exp:match(input)
 end
 
 -- Back End ---------------------------------------
 
-local function compile(ast)
+local function addCode(state, op)
+  local code = state.code
+  code[#code + 1] = op
+end
+
+local ops = { ["+"] = "add", ["-"] = "sub", ["*"] = "mul", ["/"] = "div" }
+
+local function codeExp(state, ast)
   if ast.tag == "number" then
-    return { "push", ast.val }
+    addCode(state, "push")
+    addCode(state, ast.val)
+  elseif ast.tag == "binop" then
+    codeExp(state, ast.e1)
+    codeExp(state, ast.e2)
+    addCode(state, ops[ast.op])
+  else
+    error("invalid tree")
   end
+end
+
+local function compile(ast)
+  local state = { code = {} }
+  codeExp(state, ast)
+  return state.code
 end
 
 -- Execution --------------------------------------
@@ -34,11 +75,20 @@ local function run(code, stack)
   local pc = 1
   local top = 0
 
-  while pc < #code do
+  while pc <= #code do
     if code[pc] == "push" then
+      print(code[pc], code[pc + 1])
       pc = pc + 1
       top = top + 1
       stack[top] = code[pc]
+    elseif code[pc] == "mul" then
+      print(code[pc], stack[top - 1], stack[top])
+      stack[top - 1] = stack[top - 1] * stack[top]
+      top = top - 1
+    elseif code[pc] == "div" then
+      print(code[pc], stack[top - 1], stack[top])
+      stack[top - 1] = stack[top - 1] / stack[top]
+      top = top - 1
     else
       error("unknown instruction")
     end
@@ -61,13 +111,20 @@ end
 function TestNumbers()
   luaunit.assertEquals(testNumbers("5"), 5)
   luaunit.assertEquals(testNumbers("10"), 10)
+
   luaunit.assertEquals(testNumbers("0x1"), 1)
-  luaunit.assertEquals(testNumbers("0x2A"), 42)
-  luaunit.assertEquals(testNumbers("0x3abc"), 15036)
-  luaunit.assertEquals(testNumbers("0x4ABCD"), 306125)
+  luaunit.assertEquals(testNumbers("0x2a"), 42)
+  luaunit.assertEquals(testNumbers("0x3de"), 990)
+  luaunit.assertEquals(testNumbers("0x4adef"), 306671)
   luaunit.assertEquals(testNumbers("0x5abcde"), 5946590)
-  luaunit.assertEquals(testNumbers("0x6F4F3F"), 7294783)
-  luaunit.assertEquals(testNumbers("    0X111"), 273)
+
+  luaunit.assertEquals(testNumbers("5 * 5"), 25)
+  luaunit.assertEquals(testNumbers("4 / 2"), 2.0)
+  luaunit.assertEquals(testNumbers("3 * 4 / 2"), 6.0)
+
+  luaunit.assertEquals(testNumbers("0x5 * 0x5"), 25)
+  luaunit.assertEquals(testNumbers("0x4 / 0x2"), 2.0)
+  luaunit.assertEquals(testNumbers("0x3 * 0x4 / 0x2"), 6.0)
 end
 
 os.exit(luaunit.LuaUnit.run())
